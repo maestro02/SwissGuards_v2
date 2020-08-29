e561e261f15a63d2ac140c8b31b9a47a1502b3c0
maestro02 <gitHub@maestro02.ch>
2020-08-29 12:40:24 +0200

    [TASK] Created single methods for each data fetch

--- app/Controllers/Api/SwgohHelpCollector.php
+++ app/Controllers/Api/SwgohHelpCollector.php
@@ -3,6 +3,7 @@
 use App\Controllers\BaseController;
 use App\Helpers\SwgohHelp;
 use http\Exception;
+use PHPUnit\Util\Json;
 use stdClass;
 
 class SwgohHelpCollector extends BaseController
@@ -25,114 +26,14 @@ class SwgohHelpCollector extends BaseController
 	{
 		$swgoh = new SwgohHelp();
 
-		// Get CategoryList
-		$matchCategory = new StdClass();
-		$matchCategory->visible = true;
-		try {
-			$data['categoryList'] = $swgoh->fetchData('categoryList', 'eng_us', $matchCategory);
-			/* ToDo: Save Data to Model and Database */
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-		try {
-			$data['categoryList_de'] = $swgoh->fetchData('categoryList', 'ger_de', $matchCategory);
-			/* ToDo: Add Translations to Model and Database */
-		} catch (Exception $e) {
-			echo $e->getMessage();
-		}
-
-
-		$projectAbilityList = new StdClass();
-		$projectAbilityList->id = 1;
-		$projectAbilityList->nameKey = 1;
-		$projectAbilityList->descKey = 1;
-		$projectAbilityList->tierList = 1;
-		$projectAbilityList->cooldown = 1;
-		$projectAbilityList->icon = 1;
-		$projectAbilityList->ultimateChargeRequired = 1;
-		$projectAbilityList->updated = 1;
-		$data['abilityList'] = $swgoh->fetchData('abilityList', 'eng_us', $projectAbilityList);
-		$projectAbilityList->tierList = 0;
-		$projectAbilityList->cooldown = 0;
-		$projectAbilityList->icon = 0;
-		$projectAbilityList->ultimateChargeRequired = 0;
-		$projectAbilityList->updated = 0;
-		$data['abilityList_de'] = $swgoh->fetchData('abilityList', 'ger_de', $projectAbilityList);
-
-
-		$projectEquipmentList = new StdClass();
-		$projectEquipmentList->id = 1;
-		$projectEquipmentList->nameKey = 1;
-		$projectEquipmentList->iconKey = 1;
-		$projectEquipmentList->requiredLevel = 1;
-		$projectEquipmentList->equipmentStat = 1;
-		$projectEquipmentList->recipeId = 1;
-		$projectEquipmentList->tier = 1;
-		$projectEquipmentList->sellValue = 1;
-		$projectEquipmentList->mark = 1;
-		$projectEquipmentList->raidLookupList = 1;
-		$projectEquipmentList->actionLinkLookupList = 1;
-		$projectEquipmentList->requiredRarity = 1;
-		$projectEquipmentList->updated = 1;
-		$data['equipmentList'] = $swgoh->fetchData('equipmentList', 'eng_us', $projectEquipmentList);
-		$equipmentList_de = new StdClass();
-		$equipmentList_de->id = 1;
-		$equipmentList_de->nameKey = 1;
-		$equipmentList_de->iconKey = 1;
-		$data['equipmentList_de'] = $swgoh->fetchData('equipmentList', 'ger_de', $equipmentList_de);
-
-
-		$data['categoryList'] = $swgoh->fetchData('materialList');
-		$categoryList_de = new StdClass();
-		$categoryList_de->id = 1;
-		$categoryList_de->nameKey = 1;
-		$categoryList_de->descKey = 1;
-		$data['categoryList_de'] = $swgoh->fetchData('materialList', 'ger_de', $categoryList_de);
-
-
-		$projectPlayerTitleList = new StdClass();
-		$projectPlayerTitleList->id = 1;
-		$projectPlayerTitleList->nameKey = 1;
-		$projectPlayerTitleList->descKey = 1;
-		$projectPlayerTitleList->shortDescLevel = 1;
-		$projectPlayerTitleList->updated = 1;
-		$data['$projectPlayerTitleList'] = $swgoh->fetchData('playerTitleList', 'eng_us', $projectPlayerTitleList);
-		$data['$projectPlayerTitleList_de'] = $swgoh->fetchData('playerTitleList', 'ger_de', $projectPlayerTitleList);
-
-
-		$data['recipeList'] = $swgoh->fetchData('recipeList');
-		$recipeList_de = new StdClass();
-		$recipeList_de->id = 1;
-		$recipeList_de->descKey = 1;
-		$data['recipeList_de'] = $swgoh->fetchData('recipeList', 'ger_de', $recipeList_de);
-
-
-		$data['skillList'] = $swgoh->fetchData('skillList');
-		$skillList_de = new StdClass();
-		$skillList_de->id = 1;
-		$skillList_de->descKey = 1;
-		$skillList_de->tierList = 1;
-		$data['skillList_de'] = $swgoh->fetchData('skillList', 'ger_de', $skillList_de);
-
-
-		$data['territoryBattleDefinitionList'] = $swgoh->fetchData('territoryBattleDefinitionList', 'eng_us');
-		$data['territoryBattleDefinitionList_de'] = $swgoh->fetchData('territoryBattleDefinitionList', 'eng_us');
-
-
-		$matchUnitsList = new StdClass();
-		$matchUnitsList->rarity = 7;
-		$matchUnitsList->obtainableTime = 0;
-		$projectUnitsList = new StdClass();
-		$projectUnitsList->id = 1;
-		$projectUnitsList->baseId = 1;
-		$projectUnitsList->nameKey = 1;
-		$projectUnitsList->descKey = 1;
-		$projectUnitsList->forceAlignment = 1;
-		$projectUnitsList->categoryIdList = 1;
-		$projectUnitsList->combatType = 1;
-		$data['unitsList'] = $swgoh->fetchData('unitsList', 'eng_us', $matchUnitsList, $projectUnitsList);
-		$data['unitsList_de'] = $swgoh->fetchData('unitsList', 'ger_de', $matchUnitsList, $projectUnitsList);
-
+		$this->getCategoryList($swgoh);
+		// $this->getAbilityList($swgoh);
+		// $this->getEquipmentList($swgoh);
+		// $this->getMaterialList($swgoh);
+		// $this->getPlayerTitle($swgoh);
+		// $this->getRecipeList($swgoh);
+		// $this->getSkillList($swgoh);
+		// $this->getUnitsList($swgoh);
 
 		/*
 		 *
@@ -145,24 +46,275 @@ class SwgohHelpCollector extends BaseController
 		 *
 		 */
 
-		return view('welcome_message', $data);
+		// return view('welcome_message');
 
 	}
 
 	public function getGuildData()
 	{
 		$swgoh = new SwgohHelp();
+		$this->getGuild($swgoh);
+		return view('welcome_message');
+	}
 
-		$guild = $swgoh->fetchGuild(256163745);
-		$data['guild'] = $guild;
-		$object = json_decode($guild);
-		$members = $object[0]->roster;
+
+	private function getGuild(SwgohHelp $swgoh):void
+	{
+		try {
+			$guild = $swgoh->fetchGuild(256163745);
+			$object = json_decode($guild);
+			/* ToDo: Save Data to Model and Database */
+			$this->getGuildMember($swgoh, $object);
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getGuildMember(SwgohHelp $swgoh, $json):void
+	{
+		$members = $json[0]->roster;
 		foreach ($members as $member) {
 			$list .= ', '.$member->allyCode;
 		}
-		$data['members'] = $swgoh->fetchPlayer($list);
+		try {
+			$result_en = $swgoh->fetchPlayer($list);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getCategoryList(SwgohHelp $swgoh)
+	{
+		// Get CategoryList
+		$match = new StdClass();
+		$match->visible = true;
+		try {
+			$result_en = $swgoh->fetchData('categoryList', 'eng_us', $match);
+			$data['result_en'] = $result_en;
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			$data['result_en'] = $e->getMessage();
+		}
+		try {
+			$result_de = $swgoh->fetchData('categoryList', 'ger_de', $match);
+			$data['result_de'] = $result_de;
+			/* ToDo: Add Translations to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			$data['result_de'] =  $e->getMessage();
+		}
 
 		return view('welcome_message', $data);
+	}
+
+	private function getAbilityList(SwgohHelp $swgoh):void
+	{
+		$projectCriteria = new StdClass();
+		$projectCriteria->id = 1;
+		$projectCriteria->nameKey = 1;
+		$projectCriteria->descKey = 1;
+		$projectCriteria->tierList = 1;
+		$projectCriteria->cooldown = 1;
+		$projectCriteria->icon = 1;
+		$projectCriteria->ultimateChargeRequired = 1;
+		$projectCriteria->updated = 1;
+		try {
+			$result_en = $swgoh->fetchData('abilityList', 'eng_us', $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		$projectCriteria->tierList = 0;
+		$projectCriteria->cooldown = 0;
+		$projectCriteria->icon = 0;
+		$projectCriteria->ultimateChargeRequired = 0;
+		$projectCriteria->updated = 0;
+		try {
+			$result_de = $swgoh->fetchData('abilityList', 'ger_de', $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getEquipmentList(SwgohHelp $swgoh):void
+	{
+		$projectCriteria = new StdClass();
+		$projectCriteria->id = 1;
+		$projectCriteria->nameKey = 1;
+		$projectCriteria->iconKey = 1;
+		$projectCriteria->requiredLevel = 1;
+		$projectCriteria->equipmentStat = 1;
+		$projectCriteria->recipeId = 1;
+		$projectCriteria->tier = 1;
+		$projectCriteria->sellValue = 1;
+		$projectCriteria->mark = 1;
+		$projectCriteria->raidLookupList = 1;
+		$projectCriteria->actionLinkLookupList = 1;
+		$projectCriteria->requiredRarity = 1;
+		$projectCriteria->updated = 1;
+		try {
+			$result_en = $swgoh->fetchData('equipmentList', 'eng_us', $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		$projectCriteria_de = new StdClass();
+		$projectCriteria_de->id = 1;
+		$projectCriteria_de->nameKey = 1;
+		$projectCriteria_de->iconKey = 1;
+		try {
+			$result_de = $swgoh->fetchData('equipmentList', 'ger_de', $projectCriteria_de);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getMaterialList(SwgohHelp $swgoh):void
+	{
+		$result_en = $swgoh->fetchData('materialList');
+		$projectCriteria_de = new StdClass();
+		$projectCriteria_de->id = 1;
+		$projectCriteria_de->nameKey = 1;
+		$projectCriteria_de->descKey = 1;
+		$result_de = $swgoh->fetchData('materialList', 'ger_de', $projectCriteria_de);
+	}
+
+	private function getPlayerTitle(SwgohHelp $swgoh):void
+	{
+		$projectCriteria = new StdClass();
+		$projectCriteria->id = 1;
+		$projectCriteria->nameKey = 1;
+		$projectCriteria->descKey = 1;
+		$projectCriteria->shortDescLevel = 1;
+		$projectCriteria->updated = 1;
+		try {
+			$result_en = $swgoh->fetchData('playerTitleList', 'eng_us', $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		try {
+			$result_de = $swgoh->fetchData('playerTitleList', 'ger_de', $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getRecipeList(SwgohHelp $swgoh):void
+	{
+		try {
+			$result_en = $swgoh->fetchData('recipeList');
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		$projectCriteria_de = new StdClass();
+		$projectCriteria_de->id = 1;
+		$projectCriteria_de->descKey = 1;
+		try {
+			$result_de = $swgoh->fetchData('recipeList', 'ger_de', $projectCriteria_de);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getSkillList(SwgohHelp $swgoh):void
+	{
+		try {
+			$result_en = $swgoh->fetchData('skillList');
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		$projectCriteria_de = new StdClass();
+		$projectCriteria_de->id = 1;
+		$projectCriteria_de->descKey = 1;
+		$projectCriteria_de->tierList = 1;
+		try {
+			$result_de = $swgoh->fetchData('skillList', 'ger_de', $projectCriteria_de);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getUnitsList(SwgohHelp $swgoh):void
+	{
+		$match = new StdClass();
+		$match->rarity = 7;
+		$match->obtainableTime = 0;
+		$projectCriteria = new StdClass();
+		$projectCriteria->id = 1;
+		$projectCriteria->baseId = 1;
+		$projectCriteria->nameKey = 1;
+		$projectCriteria->descKey = 1;
+		$projectCriteria->forceAlignment = 1;
+		$projectCriteria->categoryIdList = 1;
+		$projectCriteria->combatType = 1;
+		$projectCriteria->skillReferenceList = 1;
+		$projectCriteria->unitTierList = 1;
+		$projectCriteria->relicDefinition = 1;
+		$projectCriteria->modRecommendationList = 1;
+		$projectCriteria->limitBreakRefList = 1;
+		$projectCriteria->crewList = 1;
+		$projectCriteria->updated = 1;
+		try {
+			$result_en = $swgoh->fetchData('unitsList', 'eng_us', $match, $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		$projectCriteria_de = new StdClass();
+		$projectCriteria_de->id = 1;
+		$projectCriteria_de->baseId = 1;
+		$projectCriteria_de->nameKey = 1;
+		$projectCriteria_de->descKey = 1;
+		try {
+			$result_de = $swgoh->fetchData('unitsList', 'ger_de', $match, $projectCriteria_de);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function getTerritoryBattle(SwgohHelp $swgoh):void
+	{
+		try {
+			$result_en = $swgoh->fetchData('territoryBattleDefinitionList', 'eng_us');
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		try {
+			$result_de = $swgoh->fetchData('territoryBattleDefinitionList', 'ger_de');
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	private function callApi(SwgohHelp $swgoh, string $listName, StdClass $projectCriteria = null, StdClass $projectCriteria_de = null, StdClass $match = null):void
+	{
+		try {
+			$result_en = $swgoh->fetchData($listName, 'eng_us', $match, $projectCriteria);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+
+		try {
+			$result_de = $swgoh->fetchData($listName, 'ger_de', $match, $projectCriteria_de);
+			/* ToDo: Save Data to Model and Database */
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+
 	}
 
 }
